f18979f7a08e61af2cde892db729f9236f476c33
eli <elzr.sr@gmail.com>
2024-01-03 14:40:56 +0100

    TextBlock shows the current sentence and go to next

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -1,10 +1,15 @@
 import '@fontsource-variable/quicksand'
+import { useState } from 'react'
 
-export default function TextBlock () {
-  const text = 'Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño.'
+export default function TextBlock ({ sentences }) {
+  const [pos, setPos] = useState(0)
+  const handleNext = () => {
+    if (pos <= sentences.length) { setPos(pos + 1) }
+  }
   return (
     <div className='w-[80em] font-qs font-[300]'>
-      <p className='text-[2.5rem] dark:text-opacity-80 dark:text-white'>{text}</p>
+      <p className='text-[2.5rem] dark:text-opacity-80 dark:text-white'>{sentences[pos]}</p>
+      <button className='p-3 text-black bg-white rounded' onClick={handleNext}>next</button>
     </div>
   )
 }
