b683c34c962551e16cbe3adb3050154388ef2ade
eli <elzr.sr@gmail.com>
2024-01-03 12:21:18 +0100

    Handle onInput in TextInput

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -8,10 +8,15 @@ export default function TextInput ({ setText }) {
   const handleBlur = (e) => {
     setTimeout(() => e.target.focus(), 1)
   }
+  const handleInput = (e) => {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+  }
   return (
     <div>
       <p className='text-[2.5rem] dark:text-opacity-80 dark:text-white'>Presiona Ctrl+V</p>
-      <input type='text' onPaste={handlePaste} className='absolute top-0 left-0 w-screen h-screen opacity-0' onBlur={handleBlur} autoFocus />
+      <input type='text' className='absolute top-0 left-0 w-screen h-screen opacity-50' onPaste={handlePaste} onBlur={handleBlur} onInput={handleInput} autoFocus />
     </div>
   )
 }
