7bea20f6381a98c15b4bed7fcbae917b9d3cf90a
eli <elzr.sr@gmail.com>
2024-01-04 19:31:04 +0100

    Separating animation from logic in TextBlock

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -5,27 +5,46 @@ import { useGSAP } from '@gsap/react'
 
 export default function TextBlock ({ sentences }) {
   const [pos, setPos] = useState(0)
+  const onNext = () => {
+    if (pos >= sentences.length) return false
+    setPos(pos + 1)
+    return true
+  }
+
+  const onPrevious = () => {
+    if (pos <= 0) return false
+    setPos(pos - 1)
+    return true
+  }
+  return (
+    <div className='w-[80em]'>
+      <Text sentence={sentences[pos]} onNext={onNext} onPrevious={onPrevious} />
+    </div>
+  )
+}
+
+function Text ({ sentence, onNext, onPrevious }) {
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown)
     return () => {
       window.removeEventListener('keydown', handleKeyDown)
     }
-  }, [pos])
+  }, [sentence])
 
-  const container = useRef(null)
+  useEffect(() => {
+    animate('+2rem')
+  }, [])
 
-  const { contextSafe } = useGSAP({ scope: container })
+  const scope = useRef(null)
+  const { contextSafe } = useGSAP({ scope })
 
-  const handleNext = contextSafe(() => {
-    if (gsap.isTweening('.good')) {
-      gsap.killTweensOf('.good')
-    }
-    if (pos < sentences.length) { setPos(pos + 1) }
-    gsap.set('.good', {
-      opacity: 0,
-      y: '+2rem'
-    })
-    gsap.to('.good', {
+  const nextKeys = ['ArrowRight', ' ', 'ArrowDown']
+  console.log('nextkeys', nextKeys)
+  const previousKeys = ['ArrowLeft', 'ArrowUp']
+
+  const animate = contextSafe((yPos) => {
+    gsap.set(scope.current, { opacity: 0, y: yPos })
+    gsap.to(scope.current, {
       opacity: 1,
       duration: 0.4,
       ease: 'power1.inOut',
@@ -33,19 +52,25 @@ export default function TextBlock ({ sentences }) {
     })
   })
 
-  const handlePrevious = () => {
-    if (pos > 0) { setPos(pos - 1) }
-  }
+  const restartAnimation = contextSafe(() => {
+    if (gsap.isTweening(scope.current)) { gsap.killTweensOf(scope.current) }
+  })
+
   const handleKeyDown = (e) => {
-    if (e.key === 'ArrowRight' || e.key === ' ') {
-      handleNext()
-    } else if (e.key === 'ArrowLeft') {
-      handlePrevious()
+    if (nextKeys.includes(e.key)) {
+      if (onNext()) {
+        restartAnimation()
+        animate('+2rem')
+      }
+    } else if (previousKeys.includes(e.key)) {
+      if (onPrevious()) {
+        restartAnimation()
+        animate('-2rem')
+      }
     }
   }
+
   return (
-    <div ref={container} className='w-[80em]'>
-      <p className='text-[2.5rem] dark:text-opacity-80 dark:text-white font-[300] font-ft good'>{sentences[pos]}</p>
-    </div>
+    <p ref={scope} className='text-[2.5rem] dark:text-opacity-80 dark:text-white font-[300] font-ft good'>{sentence}</p>
   )
 }
