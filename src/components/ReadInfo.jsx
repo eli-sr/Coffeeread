dd44b935f320f79179c6ddf0c33a97506355d9c2
eli <elzr.sr@gmail.com>
2024-01-05 20:00:07 +0100

    Line with ref

--- src/components/ReadInfo.jsx
+++ src/components/ReadInfo.jsx
@@ -1,23 +1,36 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const LinesPerPage = 35
 
 export default function ReadInfo ({ pos, lines }) {
-  const [currentLine, setCurrentLine] = useState(pos + 1)
+  const inputRef = useRef(null)
 
   useEffect(() => {
     if (pos + 1 <= lines) {
-      setCurrentLine(pos + 1)
+      inputRef.current.value = pos + 1
     }
   }, [pos])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <ul className='fixed bottom-0 right-0 flex flex-row pb-1 pr-2 space-x-2 opacity-45'>
-      <li>
-        <span>Ln: {currentLine}/{lines}</span>
+      <li className='flex flex-row'>
+        <span>Ln: </span>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='number'
+            className='ml-2 bg-transparent outline-none'
+            style={{ minWidth: (pos + 1).toString().length + 'ch' }}
+            ref={inputRef}
+          />
+        </form>
+        <span>/{lines}</span>
       </li>
       <li>
-        <span>Pg: {Math.ceil(currentLine / LinesPerPage)}/{Math.ceil(lines / LinesPerPage)}</span>
+        <span>Pg: {Math.ceil((pos + 1) / LinesPerPage)}/{Math.ceil(lines / LinesPerPage)}</span>
       </li>
     </ul>
   )
