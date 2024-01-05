3b5a77ec88e7b6847ad1035c026e9b7e4d033e91
eli <elzr.sr@gmail.com>
2024-01-05 16:11:50 +0100

    Pages counter in ReadInfo

--- src/components/ReadInfo.jsx
+++ src/components/ReadInfo.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 
+const LinesPerPage = 35
+
 export default function ReadInfo ({ pos, lines }) {
   const [currentLine, setCurrentLine] = useState(pos + 1)
 
   useEffect(() => {
-    if (pos + 1 > lines) {
-      setCurrentLine('End')
-    } else {
+    if (pos + 1 <= lines) {
       setCurrentLine(pos + 1)
     }
   }, [pos])
@@ -15,6 +15,9 @@ export default function ReadInfo ({ pos, lines }) {
     <ul className='fixed bottom-0 right-0 flex flex-row pb-1 pr-2 space-x-2 opacity-45'>
       <li>
         <span>Ln: {currentLine}/{lines}</span>
+      </li>
+      <li>
+        <span>Pg: {Math.ceil(currentLine / LinesPerPage)}/{Math.ceil(lines / LinesPerPage)}</span>
       </li>
     </ul>
   )
