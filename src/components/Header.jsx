20e4a68be2a6940cad21bbe7ebd1511d30bf2c9f
eli <elzr.sr@gmail.com>
2024-01-07 16:52:56 +0100

    Created SearchBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useTextStore } from '../store/store'
+import SearchBar from './SearchBar'
 
 export default function Header () {
   const { setText } = useTextStore()
@@ -6,9 +7,16 @@ export default function Header () {
     setText('')
   }
   return (
-    <header className='fixed top-0 h-20'>
-      <h1>read</h1>
-      <button onClick={handleNewText} className='p-2 text-black bg-white rounded'>Nuevo texto</button>
+    <header className='fixed top-0 flex justify-center w-full'>
+      <ul>
+        <li>
+          <button onClick={handleNewText} className='p-2 text-black bg-white rounded'>Nuevo texto</button>
+        </li>
+        <li>
+          <SearchBar />
+        </li>
+      </ul>
+
     </header>
   )
 }
